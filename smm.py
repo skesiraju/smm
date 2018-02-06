fixed sign projection to orthant projection. displays T sparsity at the end of training

smm.py
@@ -38,9 +38,6 @@ class SMM():
             cuda (boolean): Use GPU? (model is always initialized on CPU)
         """
 
-        # import pdb
-        # pdb.set_trace()
-
         self.cuda = cuda
         if cuda:
             self.dtype = torch.cuda.FloatTensor
@@ -55,7 +52,7 @@ class SMM():
 
         torch.manual_seed(0)  # for consistent results on CPU and GPU
         # bases or subspace or total variability matrix
-        self.T = Variable(torch.randn(V, self.K).type(self.dtype),
+        self.T = Variable(torch.randn(V, self.K).type(self.dtype) * 0.001,
                           requires_grad=True)
 
         # i-vectors
@@ -190,7 +187,7 @@ def update_ws(model, opt_w, loss, X, rng=None):
         bt_ixs = ((old_loss_d - model.nllh_d) < 0).nonzero().squeeze()
         bti += 1
         if bti == 10 and bt_ixs.dim() > 0:
-            print("BT steps > 10 for", bt_ixs.dim(), "W.")
+            print("BT steps > 10 for", bt_ixs.size()[0], "W.")
             model.W.data[:, bt_ixs] = old_w[:, bt_ixs]  # use old_w
 
             if rng:
@@ -203,18 +200,19 @@ def update_ws(model, opt_w, loss, X, rng=None):
     return loss
 
 
-def sign_projection(model, config):
-    """ Sign projection in case of L1 regularization """
+def orthant_projection(model, opt_t, config):
+    """ Orthant projection in case of L1 regularization """
 
     T = model.T.data.cpu().clone().numpy()
     grad = model.T.grad.data.cpu().clone().numpy()
 
     diff_pts = T.nonzero()
     grad[diff_pts] += (config['lam_t'] * np.sign(T[diff_pts]))
+    grad *= -1  # ascent direction
 
     # sub-gradients
     non_diff_pts = np.where(T == 0)
-    if len(non_diff_pts[0]) > 0:
+    if non_diff_pts[0].size > 0:
         for row, col in zip(non_diff_pts[0], non_diff_pts[1]):
             if grad[row, col] < -config['lam_t']:
                 grad[row, col] += config['lam_t']
@@ -226,9 +224,19 @@ def sign_projection(model, config):
                 continue
 
     if model.cuda:
-        model.T.grad.data = torch.from_numpy(grad).float().cuda()
+        model.T.grad.data = torch.from_numpy(-grad).float().cuda()
     else:
-        model.T.grad.data = torch.from_numpy(grad).float()
+        model.T.grad.data = torch.from_numpy(-grad).float()
+    opt_t.step()
+    del grad
+
+    t_new = model.T.data.cpu().clone().numpy()
+    t_new[np.where((T * t_new) < 0)] = 0.0
+
+    if model.cuda:
+        model.T.data = torch.from_numpy(t_new).float().cuda()
+    else:
+        model.T.data = torch.from_numpy(t_new).float()
 
 
 def update_ts(model, opt_t, loss, X, config):
@@ -241,9 +249,9 @@ def update_ts(model, opt_t, loss, X, config):
     loss.backward()  # get the gradients
 
     if config['reg_t'] == 'l1':
-        sign_projection(model, config)
-
-    opt_t.step()
+        orthant_projection(model, opt_t, config)
+    else:
+        opt_t.step()
 
     # Check if the updates have decreased the loss, else backtrack
     # halving the step (max 10 steps).
@@ -372,9 +380,9 @@ def update_ts_batch_wise(model, opt_t, data_loader, old_loss, config):
             loss_batch.backward()
 
             if config['reg_t'] == 'l1':
-                sign_projection(model, config)
-
-            opt_t.step()
+                orthant_projection(model, opt_t, config)
+            else:
+                opt_t.step()
 
             loss = compute_loss_batch_wise(model, data_loader, use='T')
 

train_and_clf.py
@@ -11,8 +11,8 @@ Gaussian linear classifier on 20 newsgroup dataset
 """
 
 import os
-import sys
 import argparse
+import h5py
 import numpy as np
 from sklearn.model_selection import StratifiedKFold as SKFold
 from sklearn.metrics import log_loss
@@ -39,79 +39,83 @@ def kfold_cv_dev(train_feats, train_labels, n_folds=5):
     i = 0
     for trn_ixs, dev_ixs in skf.split(train_feats, train_labels):
 
-        glc = GLC(est_prior=True)  # est class prior
-        glc.train(train_feats[trn_ixs, :], train_labels[trn_ixs])
-
-        dev_pred = glc.predict(train_feats[dev_ixs])
-        dev_prob = glc.predict(train_feats[dev_ixs], return_probs=True)
-
-        scores[i, 0] = np.mean(train_labels[dev_ixs] == dev_pred) * 100.
-        scores[i, 1] = log_loss(train_labels[dev_ixs], dev_prob)
+        scores[i, :2] = run_glc(train_feats[trn_ixs], train_labels[trn_ixs],
+                                train_feats[dev_ixs], train_labels[dev_ixs])
         i += 1
 
     return np.mean(scores[:, 0]), np.mean(scores[:, 1])
 
 
-def load_data(train_npy_f):
-    """ Load data """
+def run_glc(train_feats, train_labels, test_feats, test_labels):
+    """ Train and classify using Gaussian linear classifier """
 
-    train_feats = np.load(train_npy_f)
-    # train_labs = np.loadtxt(ARGS.train_labs_f, dtype=int)
-    train_labs = np.loadtxt('20news-bydate/matlab/train.label', dtype=int)
+    glc = GLC(est_prior=True)
+    glc.train(train_feats, train_labels)
+    test_pred = glc.predict(test_feats)
+    test_prob = glc.predict(test_feats, return_probs=True)
 
-    # test_feats = np.load(ARGS.test_npy_f)
-    test_feats = np.load(ARGS.train_npy_f.replace("train", "test"))
-    test_labs = np.loadtxt('20news-bydate/matlab/test.label', dtype=int)
-    # test_labs = np.loadtxt(ARGS.test_labs_f, dtype=int)
+    test_acc = np.mean(test_labels == test_pred) * 100.
+    test_xen = log_loss(test_labels, test_prob)
 
-    if train_feats.shape[0] != train_labs.shape[0]:
-        train_feats = train_feats.T
-
-    if test_feats.shape[0] != test_labs.shape[0]:
-        test_feats = test_feats.T
-
-    if min(train_labs) == 1:
-        train_labs -= 1
-    if min(test_labs) == 1:
-        test_labs -= 1
-
-    assert train_feats.shape[1] == test_feats.shape[1]
-
-    return train_feats, train_labs, test_feats, test_labs
+    return test_acc, test_xen
 
 
-def main():
-    """ main method """
-
-    train_f = ARGS.train_npy_f
-
-    max_iters = int(os.path.splitext(os.path.basename(
-        ARGS.train_npy_f))[0].split("_")[-1][1:])
-    print('max_iters:', max_iters)
+def run(train_h5, test_h5, max_iters, mbase):
+    """ Train and classify for every iteration of extracted i-vector """
 
     #  each row: dev_acc, dev_xen, test_acc, test_xen
     scores = np.zeros(shape=(max_iters, 4))
     scores[:, [1, 3]] = np.inf
 
+    train_labels = np.loadtxt('20news-bydate/matlab/train.label', dtype=int)
+    test_labels = np.loadtxt('20news-bydate/matlab/test.label', dtype=int)
+
+    if min(train_labels) == 1:
+        train_labels -= 1
+    if min(test_labels) == 1:
+        test_labels -= 1
+
     for i in range(1, max_iters+1):
-        pfx = "_e" + str(max_iters)
-        train_fpath = train_f.replace(pfx, "_e" + str(i))
 
-        if not os.path.exists(train_fpath):
-            continue
+        train_f = TRN_BASE + "_model_" + mbase + "_e" + str(i)
+        test_f = train_f.replace(TRN_BASE, TEST_BASE)
 
-        train_feats, train_labs, test_feats, test_labs = load_data(train_fpath)
-        scores[i-1, :2] = kfold_cv_dev(train_feats, train_labs)
+        try:
 
-        glc = GLC(est_prior=True)
-        glc.train(train_feats, train_labs)
-        test_pred = glc.predict(test_feats)
-        test_prob = glc.predict(test_feats, return_probs=True)
+            train_feats = train_h5.get(train_f).value
+            test_feats = test_h5.get(test_f).value
 
-        test_acc = np.mean(test_labs == test_pred) * 100.
-        test_xen = log_loss(test_labs, test_prob)
+            if train_feats.shape[0] != train_labels.shape[0]:
+                train_feats = train_feats.T
 
-        scores[i-1, 2:] = test_acc, test_xen
+            if test_feats.shape[0] != test_labels.shape[0]:
+                test_feats = test_feats.T
+
+            scores[i-1, :2] = kfold_cv_dev(train_feats, train_labels)
+
+            scores[i-1, 2:] = run_glc(train_feats, train_labels, test_feats, test_labels)
+
+        except AttributeError:
+            pass
+
+    return scores
+
+
+def main():
+    """ main method """
+
+    ivecs_h5f = os.path.realpath(ARGS.ivecs_h5)
+    h5f = h5py.File(ivecs_h5f, 'r')
+    train_h5 = h5f.get('train')
+    test_h5 = h5f.get('test')
+
+    max_iters = int(os.path.splitext(os.path.basename(
+        ARGS.ivecs_h5))[0].split("_")[-1][1:])
+    print('max_iters:', max_iters)
+    mbase = os.path.splitext(os.path.basename(ivecs_h5f))[0].split("_")[-2]
+    print('mbase:', mbase)
+
+    scores = run(train_h5, test_h5, max_iters, mbase)
 
     dev_acc_ix = np.argmax(scores[:, 0])
     dev_xen_ix = np.argmin(scores[:, 1])
@@ -119,20 +123,30 @@ def main():
     print("Best dev accuracy:", scores[dev_acc_ix], dev_acc_ix+1)
     print("Best dev Xentropy:", scores[dev_xen_ix], dev_xen_ix+1)
 
-    base = os.path.splitext(os.path.basename(ARGS.train_npy_f))[0]
-    res_f = os.path.realpath(os.path.dirname(ARGS.train_npy_f) + "/../results.txt")
+    base = os.path.splitext(os.path.basename(ARGS.ivecs_h5))[0]
+    res_f = os.path.realpath(os.path.dirname(ARGS.ivecs_h5) + "/../results.txt")
     header = "\ndev_acc,dev_xen,test_acc,test_xen"
-    with open(res_f, 'ab') as fpw:
+    if ARGS.ovr:
+        mode = 'wb'
+    else:
+        mode = 'ab'
+    with open(res_f, mode) as fpw:
         np.savetxt(fpw, scores, fmt='%.4f', header=base+header)
     print("Saved to", res_f)
+
+    with open(res_f.replace('results.txt', 'best_score.txt'), 'w') as fpw:
+        np.savetxt(fpw, scores[dev_acc_ix], fmt='%.4f', header=header[1:])
 
 
 if __name__ == "__main__":
 
     PARSER = argparse.ArgumentParser(description=__doc__)
-    PARSER.add_argument("train_npy_f", help="path to train feats")
-    # PARSER.add_argument("train_labs_f", help="path to train labels")
-    # PARSER.add_argument("test_npy_f", help="path to test feats")
-    # PARSER.add_argument("test_labs_f", help="path to test labels")
+    PARSER.add_argument("ivecs_h5", help="path to ivecs.h5 file")
+    PARSER.add_argument("--ovr", action="store_true",
+                        help="over-write results file")
     ARGS = PARSER.parse_args()
+
+    TRN_BASE = 'train'
+    TEST_BASE = 'test'
+
     main()

utils.py
@@ -17,6 +17,7 @@ import codecs
 import subprocess
 import tempfile
 from collections import OrderedDict
+import h5py
 import torch
 import numpy as np
 import scipy.io as sio
@@ -168,6 +169,7 @@ def merge_ivecs(ivecs_dir, sbase, mbase, xtr_iters, n_batches):
         n_batches (int): number of batches
     """
 
+    print("Merging i-vectors..")
     data = []
     out_f = ivecs_dir + sbase
     for i in range(xtr_iters):
@@ -178,7 +180,48 @@ def merge_ivecs(ivecs_dir, sbase, mbase, xtr_iters, n_batches):
                 data.append(np.load(fname))
                 subprocess.check_call("rm -rf " + fname, shell=True)
 
-        if len(data) > 0:
+        if data:
             np.save(out_f + "_" + mbase + "_e" + str(i+1) + ".npy",
                     np.concatenate(data, axis=0))
             data = []
+
+
+def save_ivecs_to_h5(in_ivecs_dir, mbase, xtr_iters):
+    """ Save all ivecs to h5 format to save disk space. """
+
+    print("Compressing i-vectors..")
+    in_files = os.listdir(in_ivecs_dir)
+
+    out_file = in_ivecs_dir + "ivecs_" + mbase + "_e" + str(xtr_iters) + ".h5"
+    h5f = h5py.File(out_file, 'w')
+
+    grp_train = h5f.create_group('train')
+    grp_test = h5f.create_group('test')
+
+    for in_file in in_files:
+
+        base, ext = os.path.splitext(os.path.basename(in_file))
+
+        if ext == ".npy":
+
+            data = np.load(in_ivecs_dir + in_file)
+
+            if "train" in base:
+                grp_train.create_dataset(base, data=data, compression='gzip')
+            else:
+                grp_test.create_dataset(base, data=data, compression='gzip')
+
+    h5f.close()
+
+    print("All ivectors saved to", out_file)
+    subprocess.check_call("rm -rf " + in_ivecs_dir + "*.npy", shell=True)
+    print("Deleted npy files.")
+
+
+def t_sparsity(model):
+    """ Compute sparsity ratio in T """
+
+    V, K = model.T.size()
+    nnz = np.count_nonzero(model.T.data.cpu().clone().numpy())
+    s_rat = ((V * K) - nnz) / (V * K)
+    return s_rat